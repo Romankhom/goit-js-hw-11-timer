40cb01e86bd5689ff1cd20e8a3cd516b66916ff4
Romankhom <homenko.r.v@gmail.com>
2020-06-29 01:20:57 +0300

    timer

--- js/index.js
+++ js/index.js
@@ -1,63 +1,41 @@
+"use strict";
+
 class CountdownTimer {
-  constructor(selector, targetDate) {
-    this.selector = selector;
-    this.targetDate = targetDate.getTime();
-    this.intervalId = 0;
-    this.refs = {
-      mainBlock: document.querySelector(this.selector),
-      days: document.querySelector(`${this.selector} span[data-value="days"]`),
-      hours: document.querySelector(
-        `${this.selector} span[data-value="hours"]`
-      ),
-      mins: document.querySelector(`${this.selector} span[data-value="mins"]`),
-      secs: document.querySelector(`${this.selector} span[data-value="secs"]`)
-    };
+  constructor(obj) {
+    this.querySelector = obj.selector;
+    this.date = obj.targetDate;
+
+    this.writeTime();
   }
 
-  timeCalculate(time) {
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    );
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
+  timeCalculate() {
+    const time = Date.parse(this.date) - Date.now();
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
     return { days, hours, mins, secs };
   }
+  writeTime() {
 
-  timeToPage({ days, hours, mins, secs }) {
-    this.refs.days.textContent = days;
-    this.refs.hours.textContent = hours;
-    this.refs.mins.textContent = mins;
-    this.refs.secs.textContent = secs;
-  }
+    const days = document.querySelector('span[data-value="days"]');
+    const hours = document.querySelector('span[data-value="hours"]');
+    const mins = document.querySelector('span[data-value="mins"]');
+    const secs = document.querySelector('span[data-value="secs"]');
 
-  timerReady() {
-    const currentDate = Date.now();
-    this.deltaTime = this.targetDate - currentDate;
-    const calculatedTime = this.timeCalculate(this.deltaTime);
-    this.timeToPage(calculatedTime);
 
-    if (this.deltaTime <= 0) {
-      this.deltaTime = 0;
-      this.timeToPage(this.timeCalculate(this.deltaTime));
-      clearInterval(this.intervalId);
-    }
-  }
-
-  counter() {
-    this.timerReady();
-
-    this.intervalId = setInterval(() => {
-      this.timerReady();
+    setInterval(() => {
+      const currentTime = this.timeCalculate();
+      days.innerText =
+        currentTime.days.toString().split('').length < 2
+          ? ('00' + currentTime.days).slice(-2)
+          : currentTime.days;
+      hours.innerText = ('00' + currentTime.hours).slice(-2);
+      mins.innerText = ('00' + currentTime.mins).slice(-2);
+      secs.innerText = ('00' + currentTime.secs).slice(-2);
     }, 1000);
-  }
-
-  pad(value) {
-    return String(value).padStart(2, "0");
   }
 }
 
-const timer = new CountdownTimer("#timer-1", new Date("Aug 24, 2020"));
+const timer = new CountdownTimer({ selector: "#timer-1", targetDate: new Date("Aug 24, 2020") });
 
-timer.counter();
